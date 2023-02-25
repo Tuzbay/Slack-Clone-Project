03ae614f9880fd023f32da624780f45fd182660e
Tuzbay <t.uzbayyelce@gmail.com>
2023-02-25 18:12:24 +0300

    Chat&Message and React Router added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Chat from "./components/Chat/Chat";
+
 function App() {
   return (
     <div className="app">
-      <Header />
-      <div className="app__body">
-        <Sidebar />
-      </div>
+      <Router>
+        <Header />
+        <div className="app__body">
+          <Sidebar />
+          <Routes>
+            <Route
+              path="/room/:roomId"
+              element={
+                <>
+                  <Chat />
+                </>
+              }
+            />
+            <Route path="/" element={<h1>HomePage</h1>} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
